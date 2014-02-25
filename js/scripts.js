ac14d852385f2533f0109e73a6618e2f24dc5c17
Natalie Blackburn & Liz Tom <student@epicodus.com>
2014-02-25 14:19:44 -0800

    validate phone number

--- js/scripts.js
+++ js/scripts.js
@@ -13,25 +13,52 @@ var Address = {
 var Phone = {
   phoneNumber: function() {
     return this.areaCode + "-" + this.firstThreeNumbers + "-" + this.lastFourNumbers;
-  }
+  },
+
+  valid: function() {
+    if ((this.areaCode.length === 3) && (this.firstThreeNumbers.length === 3) && (this.lastFourNumbers.length === 4)) {
+      if ((this.areaCode % 1 !== 0) || (this.firstThreeNumbers % 1 !== 0) || (this.lastFourNumbers % 1 !== 0)) {
+        return false;
+      } 
+    } else {
+      return false;
+    } 
+    return true;
+  } 
 };
 
 $(document).ready(function() {
   $("#add-address").click(function() {
-    $("#new-addresses").append('<div class="new-address">' +
+    $("#new-addresses").append('Next Address' + '<div class="new-address">' +
                                   '<div class="form-group">' +
-                                    '<label for="new-street">Street</label>' +
-                                    '<input type="text" class="form-control new-street">' +
+                                    '<input type="text" class="form-control new-street" placeholder="Street">' +
                                   '</div>' +
                                   '<div class="form-group">' +
-                                    '<label for="new-city">City</label>' +
-                                    '<input type="text" class="form-control new-city">' +
+                                    '<input type="text" class="form-control new-city" placeholder="City">' +
                                   '</div>' +
                                   '<div class="form-group">' +
-                                    '<label for="new-state">State</label>' +
-                                    '<input type="text" class="form-control new-state">' +
+                                    '<input type="text" class="form-control new-state" placeholder="State">' +
                                   '</div>' +
-                                '</div>')
+                                '</div>') 
+  });
+  $("#add-phone").click(function() {
+    $("#new-phone-numbers").append('<div class="new-phone">' +
+                                      '<div class="col-xs-3">' +
+                                        '<div class="form-group">' +
+                                          '<input type="text" class="form-control new-area-code"  placeholder="Area Code">' +
+                                        '</div>' +
+                                      '</div>' +
+                                      '<div class="col-xs-4">  ' +
+                                        '<div class="form-group">' +
+                                          '<input type="text" class="form-control new-first-three" placeholder="First Three Digits">' +
+                                        '</div>' +
+                                      '</div>' +
+                                      '<div class="col-xs-5">' +
+                                        '<div class="form-group">' +
+                                          '<input type="text" class="form-control new-last-four"  placeholder="Last Four Digits">' +
+                                        '</div>' +
+                                      '</div>' +
+                                    '</div>')
   });
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
@@ -43,21 +70,20 @@ $(document).ready(function() {
     newContact.firstName = inputtedFirstName;
     newContact.lastName = inputtedLastName;
 
-
     newContact.phoneNumbers = [];
     newContact.addresses = [];
+
+    var newPhone = Object.create(Phone);
 
     $(".new-phone").each(function() {
       var inputtedAreaCode = $(this).find("input.new-area-code").val();
       var inputtedFirstThree = $(this).find("input.new-first-three").val();
       var inputtedLastFour = $(this).find("input.new-last-four").val();
-
-      var newPhone = Object.create(Phone);
-      newPhone.areaCode = inputtedAreaCode;
-      newPhone.firstThreeNumbers = inputtedFirstThree;
-      newPhone.lastFourNumbers = inputtedLastFour;
-
-      newContact.phoneNumbers.push(newPhone);
+  
+        newPhone.areaCode = inputtedAreaCode;
+        newPhone.firstThreeNumbers = inputtedFirstThree;
+        newPhone.lastFourNumbers = inputtedLastFour;
+        newContact.phoneNumbers.push(newPhone);
     });
 
     $(".new-address").each(function() {
@@ -73,26 +99,31 @@ $(document).ready(function() {
       newContact.addresses.push(newAddress);
     });
 
+    if (newPhone.valid()) {
+      $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+      $(".contact").last().click(function() {
+      
+        $("#show-contact").show();
 
-    $(".contact").last().click(function() {
-      $("#show-contact").show();
+        $("#show-contact h2").text(newContact.fullName());
+        $(".first-name").text(newContact.firstName);
+        $(".last-name").text(newContact.lastName);
 
-      $("#show-contact h2").text(newContact.fullName());
-      $(".first-name").text(newContact.firstName);
-      $(".last-name").text(newContact.lastName);
+        $("ul#phone-numbers").text("");
+        newContact.phoneNumbers.forEach(function(phone) {
+          $("ul#phone-numbers").append("<li>" + phone.phoneNumber() + "</li>");
+        });
 
-      $("ul#phone-numbers").text("");
-      newContact.phoneNumbers.forEach(function(phone) {
-        $("ul#phone-numbers").append("<li>" + phone.phoneNumber() + "</li>");
+        $("ul#addresses").text("");
+        newContact.addresses.forEach(function(address) {
+          $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
+        });
       });
-
-      $("ul#addresses").text("");
-      newContact.addresses.forEach(function(address) {
-        $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
-      });
-    });
+      } else {
+        alert("Please Enter a Valid Number.");  
+      }
+    
 
     this.reset();
   });
--- spec/specs.js
+++ spec/specs.js
@@ -8,6 +8,12 @@ describe("Address", function () {
       testAddress.fullAddress().should.equal("123 4th Ave, Portland, Oregon");
     });
   });
+  // describe("invalid", function() {
+  //   it("returns invalid if the information entered does not constitue a valid address", function() {
+  //     var testAddress = Object.create(Address);
+  //     testAddress.street = 
+  //   })
+  // }
 });
 
 describe("Phone", function(){
@@ -18,6 +24,15 @@ describe("Phone", function(){
       testPhone.firstThreeNumbers = "551";
       testPhone.lastFourNumbers = "1234"; 
       testPhone.phoneNumber().should.equal("545-551-1234");
+    });
+  });
+  describe("valid", function() {
+    it("returns invalid for an entry containing non-number characters", function() {
+      var testPhone = Object.create(Phone);
+      testPhone.areaCode = "332";
+      testPhone.firstThreeNumbers = "3445";
+      testPhone.lastFourNumbers = "5698";
+      testPhone.valid().should.equal(false);
     });
   });
 });
